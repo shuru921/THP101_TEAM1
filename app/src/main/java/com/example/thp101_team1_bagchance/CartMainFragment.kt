9682b51cbe9078951bb0b7efb06b6f7ac35dcc4e
hsing <samuel980428@gmail.com>
2023-05-13 13:52:39 +0800

    建主頁面

---
+++ app/src/main/java/com/example/thp101_team1_bagchance/CartMainFragment.kt
@@ -0,0 +1,31 @@
+package com.example.thp101_team1_bagchance
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class CartMainFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = CartMainFragment()
+    }
+
+    private lateinit var viewModel: CartMainViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_cart_main, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(CartMainViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
