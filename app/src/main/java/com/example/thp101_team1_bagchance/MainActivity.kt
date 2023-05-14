f50153f99b3af799d4ded272155e4a47d8a88ed7
Ruby <shuru921@gmail.com>
2023-05-14 15:32:10 +0800

    新增bottombar以及修改settingfragment、chatfragment

--- app/src/main/java/com/example/thp101_team1_bagchance/MainActivity.kt
+++ app/src/main/java/com/example/thp101_team1_bagchance/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.example.thp101_team1_bagchance
 
+import android.os.Binder
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.example.thp101_team1_bagchance.databinding.ActivityUserBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityUserBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityUserBinding.inflate(LayoutInflater.from(this))
+        setContentView(binding.root)
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        // 頁面切換可以整合BottomNavigationView功能
+        NavigationUI.setupWithNavController(
+            binding.bottomNavigationView,
+            navHostFragment.navController
+        )
     }
 }
