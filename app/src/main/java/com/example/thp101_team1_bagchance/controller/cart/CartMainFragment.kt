781dae3cc7722579904b43ce52364225fc4c21e3
hsing <samuel980428@gmail.com>
2023-05-14 11:45:38 +0800

    測試會否

--- app/src/main/java/com/example/thp101_team1_bagchance/controller/cart/CartMainFragment.kt
+++ app/src/main/java/com/example/thp101_team1_bagchance/controller/cart/CartMainFragment.kt
@@ -10,7 +10,7 @@ import com.example.thp101_team1_bagchance.R
 import com.example.thp101_team1_bagchance.viewmodel.cart.CartMainViewModel
 
 class CartMainFragment : Fragment() {
-
+//test
     companion object {
         fun newInstance() = CartMainFragment()
     }
