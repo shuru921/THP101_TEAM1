1c17ea63be6148a6b124e76c51d83dd360f136b1
Gary <liu919045@gmail.com>
2023-05-14 12:13:42 +0800

    新增post fragment

---
+++ app/src/main/java/com/example/thp101_team1_bagchance/controller/post/PostAlbumFragment.kt
@@ -0,0 +1,12 @@
+package com.example.thp101_team1_bagchance.controller.post
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class PostAlbumFragment : Fragment() {
+
+
+}
