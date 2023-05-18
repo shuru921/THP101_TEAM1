c7395574f23e70527e3d3ea26fe8643177b3623f
Yo <yoyo79791212@gmail.com>
2023-05-18 15:50:40 +0800

    add explore coding

---
+++ app/src/main/java/com/example/thp101_team1_bagchance/viewmodel/explore/ExploreStory.kt
@@ -0,0 +1,5 @@
+package com.example.thp101_team1_bagchance.viewmodel.explore
+
+import java.io.Serializable
+
+class ExploreStory (var exlporeimageId: Int, var explorename: String, var exploredate: String, var storytext: String,var storypic: Int,var storymes:String,var storymessent:Int) : Serializable
