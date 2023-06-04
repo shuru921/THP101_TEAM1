8cd350cfd1a9cf1ef718d816fbed76d648faaea3
Gary <liu919045@gmail.com>
2023-06-04 11:00:51 +0800

    新增定位功能，貼文跳轉到個人頁面。

--- app/src/main/java/com/example/thp101_team1_bagchance/viewmodel/post/PostNewContentViewModel.kt
+++ app/src/main/java/com/example/thp101_team1_bagchance/viewmodel/post/PostNewContentViewModel.kt
@@ -4,5 +4,8 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class PostNewContentViewModel : ViewModel() {
-
+    val locationName: MutableLiveData<String> by lazy { MutableLiveData<String>() }
+    val textReverseGeocode: MutableLiveData<String> by lazy { MutableLiveData<String>() }
+    val text: MutableLiveData<String> by lazy { MutableLiveData<String>() }
 }
+
