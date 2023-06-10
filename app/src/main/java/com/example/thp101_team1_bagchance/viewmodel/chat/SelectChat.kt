bd7d9f5ee62c61a5cf006fa65d3ea8faf41df8e5
Howard <howard8684@gmail.com>
2023-06-10 15:10:38 +0800

    更新

--- app/src/main/java/com/example/thp101_team1_bagchance/viewmodel/chat/SelectChat.kt
+++ app/src/main/java/com/example/thp101_team1_bagchance/viewmodel/chat/SelectChat.kt
@@ -3,20 +3,23 @@ package com.example.thp101_team1_bagchance.viewmodel.chat
 import com.example.thp101_team1_bagchance.R
 import java.sql.Timestamp
 
-data class SelectChat (val id: Int,
-                       val inviteUid: Int,
-                       val beInvitedUid: Int,
-                       val status: String,
-                       val createDate: Timestamp? = null,
-                       val lastUpdateDate: Timestamp? = null,
-                       val inviteUidname: String,
-                       val beInvitedUidname: String,
-                       val inviteUidpic: ByteArray? = null,
-                       val beInvitedUidpic: ByteArray? = null,
-                       val message : String? = null,
-                       val PICTURE : ByteArray? = null,
-                       val RECORDINGPATH : String? = null,
-                       val sendUid : Int
+data class SelectChat(
+    val id: Int,
+    val inviteUid: Int,
+    val beInvitedUid: Int,
+    val status: String,
+    val createDate: Timestamp? = null,
+    val lastUpdateDate: Timestamp? = null,
+    val inviteUidname: String,
+    val beInvitedUidname: String,
+    val inviteUidpic: ByteArray? = null,
+    val beInvitedUidpic: ByteArray? = null,
+    val message: String? = null,
+    val PICTURE: ByteArray? = null,
+    val RECORDINGPATH: String? = null,
+    val sendUid: Int = 0,
+    val beInvitedUidMail: String? = null,
+    val invitedUidMail: String? = null
 ) : java.io.Serializable
 // fixme: val image :Int = R.drawable.ic_camera 是假資料串後端得刪除
 // fixme: pic如果是null看要不要給預設值(沒頭貼給個醜醜的預設)
